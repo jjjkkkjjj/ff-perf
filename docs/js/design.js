a03121851a0ca011974377005252031890f4cf73
jjjkkkjjj <kado_89_ssp_jun@yahoo.co.jp>
2021-05-12 01:36:55 +0900

    show tips

--- docs/js/design.js
+++ docs/js/design.js
@@ -6,6 +6,23 @@ $(function() {
   
         data.params.forEach(function(b) {
             $("#selparams").append('<option value="' + b.value + '">' + b.text + '</option>');
-          });
+        });
     });
+  }
+);
+
+// show/hide tips
+$(function() {
+  $('#hinticon').hover(function(){
+    $('#paramtips').fadeIn(500);
+  },function(){
+    $('#paramtips').fadeOut(500);
   });
+});
+
+
+// set paramtips position next to hinticon
+$(document).ready(function() {
+  $("#paramtips").css("left", $("#hinticon").offset().left + $("#hinticon").outerWidth());
+  $("#paramtips").css("top",  -($("#hinticon").position().top + $("#hinticon").height()));
+});
