69c74b7de27dc52caaf1d8d90facde3228dcb347
jjjkkkjjj <kado_89_ssp_jun@yahoo.co.jp>
2021-05-16 16:09:35 +0900

    show table by grid.js

--- docs/js/ff-perf.js
+++ docs/js/ff-perf.js
@@ -4,3 +4,15 @@ function showSum() {
     var sum = num1 + num2;
     alert(sum);
 }
+
+$("div#dattable").Grid({
+    search: true,
+    pagination: true,
+    sort: true,
+    columns: ['Date', 'TRIMP'],
+    data: [
+    ['2021/05/06', 25],
+    ['2021/05/07', 59],
+    // ...
+    ],
+});
