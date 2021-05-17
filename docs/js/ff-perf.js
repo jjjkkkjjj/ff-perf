2aac5bbe2390730ef4ba2f13c9c123dd0ddfafac
jjjkkkjjj <kado_89_ssp_jun@yahoo.co.jp>
2021-05-18 01:50:21 +0900

    save

--- docs/js/ff-perf.js
+++ docs/js/ff-perf.js
@@ -1,3 +1,4 @@
+
 function showSum() {
     var num1 = 1;
     var num2 = 2;
@@ -7,20 +8,41 @@ function showSum() {
 
 // table
 var clients = [
-    { "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
-    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
-    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
-    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },
-    { "Name": "Ramona Benton", "Age": 32, "Country": 3, "Address": "Ap #614-689 Vehicula Street", "Married": false }
+    { "Date": "2021/05/02", "TRIMP": 25},
+    { "Date": "2021/05/03", "TRIMP": 45},
+    { "Date": "2021/05/05", "TRIMP": 29},
 ];
 
-var countries = [
-    { Name: "", Id: 0 },
-    { Name: "United States", Id: 1 },
-    { Name: "Canada", Id: 2 },
-    { Name: "United Kingdom", Id: 3 }
-];
+// date
+// https://codepen.io/beaver71/pen/OzPXQX
+/*
+var DateField = function(config) {
+    jsGrid.Field.call(this, config);
+    };
 
+    DateField.prototype = new jsGrid.Field({
+    sorter: function(date1, date2) {
+        return new Date(date1) - new Date(date2);
+    },
+    itemTemplate: function(value) {
+        return new Date(value).toDateString();
+    },
+    insertTemplate: function(value) {
+        return this._insertPicker = $("<input>").datepicker({ defaultDate: new Date() });
+    },
+    editTemplate: function(value) {
+        return this._editPicker = $("<input>").datepicker().datepicker("setDate", new Date(value));
+    },
+    insertValue: function() {
+        return this._insertPicker.datepicker("getDate").toISOString();
+    },
+    editValue: function() {
+        return this._editPicker.datepicker("getDate").toISOString();
+    }
+    });
+
+jsGrid.fields.dateField = DateField;
+*/
 $("#dattable").jsGrid({
     width: "100%",
     height: "auto",
@@ -33,17 +55,17 @@ $("#dattable").jsGrid({
     data: clients,
 
     fields: [
-        { name: "Name", type: "text", width: 150, validate: "required" },
-        { name: "Age", type: "number", width: 50 },
-        { name: "Address", type: "text", width: 200 },
-        { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
-        { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
+        { name: "Date", type: "text", width: 150, align: "center", validate: "required" },
+        { name: "TRIMP", type: "number", width: 50, align: "center", validate: "required" },
         { type: "control" }
     ]
-  });
-  
+});
+
+
 // graph
 $(function () {
+    
+    // graph
     var chart = c3.generate({
         bindto: '#datgraph',
         size: {
@@ -64,3 +86,5 @@ $(function () {
         }
     });
 });
+
+
